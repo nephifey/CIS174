2d9c8b217927b2e443f9c78d2d99922baec67bba
Nathan Phifer <nephifer@dmacc.edu>
2019-02-06 23:45:54 -0600

    Assignment 4.1 | Add a menu | Add a layout page

---
+++ WebAppHomework2/WebAppHomework/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace WebAppHomework.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include(
+                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/jqeury-ui-{version}.js",
+                    "~/Scripts.unbotrusive.ajax.js",
+                    "~/Scripts/bootstrap.min.js"
+                    ));
+
+            bundles.Add(new StyleBundle("~/bundles/styles")
+                .Include(
+                    "~/Content/bootstrap.min.css"
+                    ));
+
+            bundles.Add(new StyleBundle("~/bundles/mainstyles")
+                .Include(
+                    "~/public/css/main.css"
+                    ));
+        }
+    }
+}
---
+++ WebAppHomework2/WebAppHomework/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppHomework.MvcApplication" Language="C#" %>
